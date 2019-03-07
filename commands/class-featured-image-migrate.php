a88925b5651367f01dcb6359311274b40faa47e6
Jim Barnes <jimbarnesdeveloper@gmail.com>
2019-03-07 10:20:49 -0500

    Updated tasks with actual logic

--- commands/class-featured-image-migrate.php
+++ commands/class-featured-image-migrate.php
@@ -5,7 +5,9 @@
 if ( ! class_exists( 'Today_Migration_Featured_Image' ) ) {
 	class Today_Migration_Featured_Image {
 		private
-			$custom_meta_field = 'the-new-custom-meta-field';
+			$custom_meta_field = 'the-new-custom-meta-field',
+			$progress,
+			$converted;
 
 		/**
 		 * Converts featured images to a custom meta field.
@@ -17,7 +19,39 @@ if ( ! class_exists( 'Today_Migration_Featured_Image' ) ) {
 		 * @when after_wp_load
 		 */
 		public function __invoke( $args ) {
-			print "Hello World";
+			$posts = get_posts( array(
+				'posts_per_page' => -1,
+				'post_status'    => 'any'
+			) );
+
+			$count = count( $posts );
+
+			$this->progress = WP_CLI\Utils\make_progress_bar(
+				"Converting featured images...",
+				$count
+			);
+
+			for ( $posts as $post ) {
+				$this->convert_featured_image( $post );
+				$this->progress.tick();
+			}
+
+			$this->progress.finish();
+			WP_CLI::success( "Converted $this->converted featured images out of $count processed posts." );
+		}
+
+		/**
+		 * Helper function that converts the featured_image
+		 * to a meta field
+		 */
+		private function convert_featured_image( $post ) {
+			$post_id  = $post->ID;
+			$image_id = get_post_thumbnail_id( $post_id );
+
+			if ( $image_id ) {
+				update_post_meta( $post_id, $this->custom_meta_field, $image_id );
+				$this->converted++;
+			}
 		}
 	}
 
--- commands/class-meta-migrate.php
+++ commands/class-meta-migrate.php
@@ -8,7 +8,8 @@ if ( ! class_exists( 'Today_Migration_Meta' ) ) {
 		private
 			$mapping = array(
 				'old-meta-key' => 'new-meta-key'
-			);
+			),
+			$progress;
 
 		/**
 		 * Converts meta fields
@@ -20,7 +21,46 @@ if ( ! class_exists( 'Today_Migration_Meta' ) ) {
 		 * @when after_wp_load
 		 */
 		public function __invoke( $args ) {
-			print "Hello World";
+			$posts = get_posts( array(
+				'posts_per_page' => -1,
+				'post_status'    => 'any'
+			) );
+
+			$count = count( $posts );
+
+			$this->progress = WP_CLI\Utils\make_progress_bar(
+				"Converting post meta...",
+				$count
+			);
+
+			for ( $posts as $post ) {
+				$this->convert_meta( $post );
+				$this->progress.tick();
+			}
+
+			$this->progress.finish();
+
+			WP_CLI::success( "Covnerted post meta for $count posts." );
+		}
+
+		/**
+		 * Helper function to convert meta fields
+		 * @author Jim Barnes
+		 * @since 1.0.0
+		 * @param WP_Post $post The post object
+		 */
+		private function convert_meta( $post ) {
+			$post_id = $post->ID;
+
+			foreach( $this->mapping as $old_key => $new_key ) {
+				$value = get_post_meta( $post_id, $old_key );
+
+				if ( count( $value ) === 1 ) {
+					$value = $value[0];
+				}
+
+				update_post_meta( $post_id, $new_key, $value );
+			}
 		}
 	}
 
