5bb65e0b5a19d04d266c43150994e4d4d0e5a3ee
Jim Barnes <jimbarnesdeveloper@gmail.com>
2019-03-07 09:44:08 -0500

    Renamed files and updated text

---
+++ today-migration-plugin.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Today Migration Plugin
+Description: Provides a series of wp cli tasks for manipulating data used by the old Today-Bootstrap theme to work with the new theme and associated plugins.
+Version: 1.0.0
+Author: UCF Web Communications
+License: GPL3
+GitHub Plugin URI: UCF/Today-Migration-Plugin
+*/
+
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
